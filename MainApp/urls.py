cd76e1cacd32311f6872b2fc0539cf6616a4c2fe
Vishnu-M18 <128456859+Vishnu-M18@users.noreply.github.com>
2023-05-08 12:24:36 +0530

    Update urls.py

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
     path('', views.index),
+    path('', include('render.urls')),
 ]
